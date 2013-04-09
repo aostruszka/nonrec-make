c0e239894cbf285b49ad3bfe573ca54b8a58bc9a
Andrzej Ostruszka <andrzej.ostruszka@gmail.com>
2013-04-09 17:24:45 +0200

    Move setting of 'd' to Makefile
    No reason to have this checked in every header so I've moved this (and
    simplified) to Makefile.
    
    In addition minor change for info message indicating end of makefiles
    reading phase.

--- mk/header.mk
+++ mk/header.mk
@@ -1,11 +1,3 @@
-ifndef d
-d := $(or $(TOP),$(shell pwd))
-dir_stack :=
-# Automatic inclusion of the skel.mk at the top level - that way
-# Rules.top has exactly the same structure as other Rules.mk
-include $(MK)/skel.mk
-endif
-
 # Clear vars used by this make system
 SRCS :=
 SRCS_EXCLUDES :=
