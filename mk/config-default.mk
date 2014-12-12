40e0033d3119c80eebe90a23d62e439edcf16fa7
Andrzej Ostruszka <andrzej.ostruszka@gmail.com>
2014-12-12 09:29:35 +0100

    Don't override environment/command line variables

--- mk/config-default.mk
+++ mk/config-default.mk
@@ -1,8 +1,18 @@
-CC := gcc
-CXX := g++
-
-# AR is a make default but won't hurt spelling it out :)
-AR := ar
-RANLIB := ranlib
+# Default values for CC/CXX/AR for GNU make are cc/g++/ar which are OK
+# for me but I spell them out here explicitly (with cc->gcc change) so
+# you can easily change them.
+# You can also override these from command line or via global variables
+ifeq ($(origin $(CC)),default)
+  CC := gcc
+endif
+ifeq ($(origin $(CXX)),default)
+  CXX := g++
+endif
+ifeq ($(origin $(AR)),default)
+  AR := ar
+endif
+ifneq ($(filter undefined default,$(origin RANLIB)),)
+  RANLIB := ranlib
+endif
 
 OPEN_GL_LIBS := -lGL -lGLU
