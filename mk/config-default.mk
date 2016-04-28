15efeacbe11ff4e0441fa3568780c4ca24190407
Andrzej Ostruszka <andrzej.ostruszka@gmail.com>
2016-04-28 09:10:14 +0200

    Fix wrong "origin" usage in config-default.mk

--- mk/config-default.mk
+++ mk/config-default.mk
@@ -2,13 +2,13 @@
 # for me but I spell them out here explicitly (with cc->gcc change) so
 # you can easily change them.
 # You can also override these from command line or via global variables
-ifeq ($(origin $(CC)),default)
+ifeq ($(origin CC),default)
   CC := gcc
 endif
-ifeq ($(origin $(CXX)),default)
+ifeq ($(origin CXX),default)
   CXX := g++
 endif
-ifeq ($(origin $(AR)),default)
+ifeq ($(origin AR),default)
   AR := ar
 endif
 ifneq ($(filter undefined default,$(origin RANLIB)),)
